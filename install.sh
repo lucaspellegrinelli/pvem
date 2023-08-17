327214207e9c6a35a7531ab9ce53933d753fb2cf
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-08-17 11:48:08 -0300

    Moved installation to exec file

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INSTALL_PATH="${HOME}/.pvem"
+
+read -p "Enter installation path [${INSTALL_PATH}]: " user_path
+
+if [ -n "${user_path}" ]; then
+    INSTALL_PATH="${user_path}"
+fi
+
+mkdir -p "${INSTALL_PATH}"
+cp pvem.sh "${INSTALL_PATH}/pvem.sh"
+echo "pvem.sh has been installed to ${INSTALL_PATH}"
+
+if ! grep -q "export PVEM_PATH=${INSTALL_PATH}" "${HOME}/.bashrc"; then
+    echo "export PVEM_PATH=${INSTALL_PATH}" >> "${HOME}/.bashrc"
+fi
+
+if ! grep -q "source ${INSTALL_PATH}/pvem.sh" "${HOME}/.bashrc"; then
+    echo "source ${INSTALL_PATH}/pvem.sh" >> "${HOME}/.bashrc"
+fi
+
+if ! grep -q "export PVEM_PATH=${INSTALL_PATH}" "${HOME}/.zshrc"; then
+    echo "export PVEM_PATH=${INSTALL_PATH}" >> "${HOME}/.zshrc"
+fi
+
+if ! grep -q "source ${INSTALL_PATH}/pvem.sh" "${HOME}/.zshrc"; then
+    echo "source ${INSTALL_PATH}/pvem.sh" >> "${HOME}/.zshrc"
+fi
